2242b21c4366d710f1976b9297a071f959d0855f
YXJ68400 <yxj68400@ucmo.edu>
2025-08-14 09:09:25 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,3 +4,32 @@ import "fmt"
 func main(){
   fmt.Println("Hello World")
 }
+
+
+func main() {
+	i := 6
+	j := &i
+	fmt.Println("Value of i:", i)
+	fmt.Println("Address of i:", &i)
+	fmt.Println("Value of j :", j)
+	fmt.Println("Value pointed to by j:", *j)
+	*j = 10
+	fmt.Println("New value of i:", i)
+}
+
+
+func main() {
+	a := 10
+	if a%2 == 0 {
+		fmt.Println(a, "is even")
+	} else {
+		fmt.Println(a, "is odd")
+	}
+}
+
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
